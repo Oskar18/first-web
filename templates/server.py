ef8ad2414ce0ba85afc07ea4916e262d5904e27e
Oskar18 <775334585@seznam.cz>
2025-05-19 20:01:51 +0200

    3

--- templates/server.py
+++ templates/server.py
@@ -1,21 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
+@app.route("/ukazka")
+def showcase():
+	name = "Adam"
+	return render_template("index.html", name=name)
+
+
+@app.route("/about-old")
+def about_old():
+	return f"<h1>Hello About</h1>"
+
+
 @app.route("/")
 def home():
-	a = 3
-	b = 4
-	return f"<h1>Hello World {a + b}</h1>"
-
+	return render_template("home.html")
 
 @app.route("/about")
 def about():
-	return f"<h1>Hello About</h1>"
+	return render_template("about.html")
+
+@app.route("/contact")
+def cantact():
+	return render_template("contact.html")
+
+
 
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True, port=5010)
